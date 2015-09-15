dfad21d148df9ce9de9e19fb7a5c3ef83d5585cd
Saurabh <saurabhjain.kjsce@gmail.com>
2015-09-15 11:54:59 -0400

    String Compression

---
+++ Cracking The Coding Interview/Chapter1/StringCompression.java
@@ -0,0 +1,34 @@
+
+public class StringCompression {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input = "abcdef";
+
+		String output = compress(input);
+		System.out.println(output);
+	}
+
+	private static String compress(String input) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < input.length() - 1; i++) {
+			count++;
+			char ch = input.charAt(i);
+			if (input.charAt(i + 1) != ch) {
+				sb.append(ch + "" + count);
+				count = 0;
+			}
+
+		}
+		String output = sb.append(input.charAt(input.length() - 1) + "" + ++count).toString();
+
+		if (output.length() > input.length())
+			return input;
+		else
+			return output;
+		// return sb.toString();
+	}
+
+}
